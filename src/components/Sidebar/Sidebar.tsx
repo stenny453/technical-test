2caad5e5286d81d4e314ae3977b1cb97bd0883a3
Stenny <stenny.manao@gmail.com>
2023-02-06 09:16:03 +0300

    sticky element

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { PropsWithChildren, useRef } from 'react';
 import useStickyElement from '../hooks/useStickyElement';
+import './sidebar.css';
 
 export default function Sidebar({ children }: PropsWithChildren) {
   const ref = useRef<HTMLElement>(null);
